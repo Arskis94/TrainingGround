6e8d535874ab31ed13775156b7c615201d7fc24e
arskis94 <arskis94@hotmail.com>
2019-10-01 14:24:49 +0300

    asd
    dsfdsf

--- JavaScript30/30 - Whack A Mole/scripts.js
+++ JavaScript30/30 - Whack A Mole/scripts.js
@@ -1,12 +1,18 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
-const scoreboard = JSON.parse(localStorage.getItem("items")) || [];
-const dbuttons = document.querySelectorAll(".button");
-const lbuttons = document.querySelectorAll(".lbutton");
-const leaderboard = document.querySelector(".theleaderBoard");
-const lederberds = leaderboard.querySelectorAll(".lederberd");
-const timer = document.querySelector(".timer");
+// const holes = document.querySelectorAll('.hole');
+// const scoreBoard = document.querySelector('.score');
+// const moles = document.querySelectorAll('.mole');
+// const leaderboardTopList = JSON.parse(localStorage.getItem("scores")) || [];
+// const dbuttons = document.querySelectorAll(".button");
+// const lbuttons = document.querySelectorAll(".lbutton");
+// const leaderboard = document.querySelector(".theleaderBoard");
+// const lederberds = leaderboard.querySelectorAll(".lederberd");
+// const timer = document.querySelector(".timer");
+// const theList = document.querySelector(".theList");
+// const lederEasy = document.querySelector(".leaderboardEasy");
+// const lederNormal = document.querySelector(".leaderboardNormal");
+// const lederHard = document.querySelector(".leaderboardHard");
+// const lederInsane = document.querySelector(".leaderboardInsane");
+// const lederGODMode = document.querySelector(".leaderboardGODMode");
 
 
 let lastHole;
@@ -38,7 +44,8 @@ function pop(popMin, popMax) {
     }, time);
 }
 
-function startGame(timeToPlay, popMin, popMax) {
+function startGame(timeToPlay, popMin, popMax, difficulty) {
+    let chosenDifficulty = `${"leaderboard"}${difficulty}`;
     name = document.querySelector(".inputtext").value;
     if (!name) {
         document.querySelector("h2").style.color = "red";
@@ -48,6 +55,7 @@ function startGame(timeToPlay, popMin, popMax) {
     document.querySelector("h2").style.color = "black";
 
     dbuttons.forEach(button => button.setAttribute("disabled", ""));
+    dbuttons.forEach(button => document.querySelector(".inputtext").disabled = true);
     timeUp = false;
     scoreBoard.textContent = 0;
     score = 0;
@@ -57,6 +65,7 @@ function startGame(timeToPlay, popMin, popMax) {
         timeUp = true;
         setTimeout(() => gameOver(), popMax);
     }, (timeToPlay + 6) * 1000);
+    displayLeaderboard(chosenDifficulty);
 }
 
 function hitMole(e) {
@@ -75,14 +84,12 @@ function theTimer(seconds, timeToPlay) {
             clearInterval(countdown);
             return;
         }
-        console.log(timeLeft);
         displayTimer(timeLeft, timeToPlay);
         timeLeft--;
     }, 1000);
 }
 
 function displayTimer(seconds, timeToPlay) {
-    //display the time before the game starts
     if (seconds > timeToPlay) {
         timer.textContent = seconds - timeToPlay
     } else if (seconds === timeToPlay) {
@@ -94,36 +101,58 @@ function displayTimer(seconds, timeToPlay) {
     }
 }
 
-function gameOver() {
-    clickedButtonValue = `${"leaderboard"}${this.textContent}`;
+function gameOver(difficulty) {
     score = scoreBoard.textContent;
+    name = document.querySelector(".inputtext").value;
     clearInterval(countdown);
-    timer.textContent = "Game is now over and your score is :" + score;
+    timer.textContent = "Game is now over and you got a score of: " + score;
     dbuttons.forEach(button => button.removeAttribute("disabled"));
-    updateLeaderboard(name, score, clickedButtonValue);
+    dbuttons.forEach(button => document.querySelector(".inputtext").disabled = false);
+    updateLeaderboard(name, score, difficulty);
 }
 
 function updateLeaderboard(name, score, difficulty) {
-    let clickedButtonValue = `${"leaderboard"}${this.textContent}`;
-    let leaderi = this.value;
-    let leader = lederberds;
-    leader[0].classList.add("active");
-    let i = 0;
-    for (i; i < lederberds.length; i++) {
-        if (leader[i].classList.contains(clickedButtonValue)) {
-            leader[i].classList.add("active");
-        } else {
-            leader[i].classList.remove("active");
-        }
+    let leaderArr = [];
+    leaderArr.push([name, score]);
+    let asd = document.querySelector(".lederbers");
+    console.log(asd);
+    if (lederEasy) {
+        lederEasy.innerHTML += `<li>${leaderArr}</li>`
+    } else if (lederNormal) {
+        lederEasy.innerHTML += `<li>${leaderArr}</li>`
     }
-    if (leader[0].classList.contains(clickedButtonValue)) {
-        console.log("OK");
+    else if (lederHard) {
+        lederEasy.innerHTML += `<li>${leaderArr}</li>`
+    }
+    else if (lederInsane) {
+        lederEasy.innerHTML += `<li>${leaderArr}</li>`
+    }
+    else if (lederGODMode) {
+        lederEasy.innerHTML += `<li>${leaderArr}</li>`
+    } else {
+        return "oopsie"
     }
     //console.log(lederberds.classList.contains("leaderboardEasy"));
 
     //lederberds.leaderboardEasy.innerHTML = "<li>name, score</li>";
 }
 
-moles.forEach(mole => mole.addEventListener("click", hitMole));
-lbuttons.forEach(button => button.addEventListener("click", updateLeaderboard));
+function displayLeaderboard( difficulty) {
+    let clickedButtonValue = `${"leaderboard"}${this.textContent}`;
+    console.log(clickedButtonValue, difficulty);
+    let leaderi = this.value;
+    let leader = lederberds;
+    leader[0].classList.add("active");
+    let i = 0;  
+    for (i; i < lederberds.length; i++) {
+        if (leader[i].classList.contains(clickedButtonValue)) {
+            leader[i].classList.add("active");
 
+        } else {
+            leader[i].classList.remove("active");
+        }
+    }
+}
+
+moles.forEach(mole => mole.addEventListener("click", hitMole));
+lbuttons.forEach(button => button.addEventListener("click", displayLeaderboard, this.value));
