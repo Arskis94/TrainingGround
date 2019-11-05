41dd3e6efc9f8f3a0fd202d0361ba99a0f086ed3
arskis94 <arskis94@hotmail.com>
2019-11-05 13:06:07 +0200

    React
    ???

---
+++ react/test/src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="navbar">Hello WorlD!</header>
+    )
+}
+
+export default Header;
---
+++ react/test/src/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <p>Some interesting stuff here</p>
+    )
+}
+
+export default MainContent
